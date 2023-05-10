b4537924b861ee476926df2eb7e2552c3098d7bc
jpmeddings <jpmeddings@mail.csuchico.edu>
2023-05-09 22:00:19 -0700

    Added Player Movement
    Added Player movement and also the camera movement. Added all the stats for the dash and the jump ability

--- FeatureProjectExploration/Assets/Scripts/PlayerMove.cs
+++ FeatureProjectExploration/Assets/Scripts/PlayerMove.cs
@@ -4,17 +4,52 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    float strafe;
-    float foward;
-    float turn;
-    float speed = 6f;
-    float turnSpeed = 10f;
+    public Camera playerCamera;
+    Transform groundCheck;
+    LayerMask groundLayer;
+    float mouseSens { get; set; } = 400f;
+    float xRotation { get; set; } = 0f;
+    [SerializeField]
+    bool isGrounded = true;
+    [SerializeField]
+    bool isJumping = false;
+    Vector3 jumpVelo;
+    float sideToSide;
+    float fowardAndBack;
 
+    //ststs
+    float moveSpeed = 6f;
+    float gravity = -21f;
+    float normalGravity;
+    float strafeGravity = -2f;
+    float jumpHeight = 1.5f;
+    float dashSpeed;
+
+    public void Start()
+    {
+        normalGravity = gravity;
+        dashSpeed = 1.1f * moveSpeed;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     public void Update()
     {
-        strafe = Input.GetAxis("Horizontal");
-        foward = Input.GetAxis("Vertical");
-
-        transform.Translate(new Vector3(strafe, 0, foward) * speed * Time.deltaTime);
+        //isGrounded = Physics.CheckSphere(groundCheck.position, .4f, groundLayer);
+        Movement();
+        MouseMovement();
+    }
+    public void Movement()
+    {
+        sideToSide = Input.GetAxis("Horizontal");
+        fowardAndBack = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(sideToSide, 0, fowardAndBack) * moveSpeed * Time.deltaTime);
+    }
+    public void MouseMovement()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        this.gameObject.transform.Rotate(Vector3.up, mouseX);
     }
 }
